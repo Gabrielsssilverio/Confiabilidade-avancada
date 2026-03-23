aee753c93bce571d925baa75ceec4471f3edd3b1
Gabrielsssilverio <gabriel1211@gmail.com>
2026-03-23 01:06:57 -0300

    Versão ultima - tentar rodar o RIA

--- FORM_eq.py
+++ FORM_eq.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-
-def example1(samples=None):
+def eq1(samples=None):
     g = np.zeros(samples.shape[0])
-    for i in range(samples.shape[0]):         
-        R = samples[i, 0]
-        S = samples[i, 1]
-        g[i] = R - S
+    for i in range(samples.shape[0]):
+        g[i] = (samples[i, 0] * samples[i, 1]) - samples[i, 2]
     return g
