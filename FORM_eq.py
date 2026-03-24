0a731ea4fd799baaf2c4470bc5598356bb19efaf
Gabrielssilverio <gabrielsilverio1211@gmail.com>
2026-03-24 17:00:37 -0300

    Continuar o looping

--- FORM_eq.py
+++ FORM_eq.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+d = 1 * (1e-0)
+
 def eq1(samples=None):
     g = np.zeros(samples.shape[0])
     for i in range(samples.shape[0]):
-        g[i] = (samples[i, 0] * samples[i, 1]) - samples[i, 2]
+        g[i] = d * ((samples[i, 0] * samples[i, 1]) - samples[i, 2])
     return g
